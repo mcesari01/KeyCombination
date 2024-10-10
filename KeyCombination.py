7901a6cb8e42c90e38eda72bf30c2e0372532d55
matteo01 <matteo.cesari2001@gmail.com>
2024-10-10 22:05:31 +0200

    add file

--- KeyCombination.py
+++ KeyCombination.py
@@ -1,18 +1,20 @@
 import tkinter as tk
 from character_map import character_map
+from word_to_symbol import word_to_symbol
 
-def find_combination(character):
-    return character_map.get(character, 'Combination not found')
+def find_combination(input_text):
+    symbol = word_to_symbol.get(input_text.lower(), input_text)
+    return character_map.get(symbol, 'Combination not found / Combinazione non trovata')
 
 def get_response():
-    entered_character = character_entry.get().strip()  # Remove any whitespace
-    key_combination = find_combination(entered_character)
+    entered_text = character_entry.get().strip()
+    key_combination = find_combination(entered_text)
     response_label.config(text=f'Key Comb.: {key_combination}')
 
 app = tk.Tk()
 app.title('Key Combination App')
 
-instruction_label = tk.Label(app, text='Enter a character:', font=('Arial', 14))
+instruction_label = tk.Label(app, text='Enter a character or word (e.g., tilde, at):', font=('Arial', 14))
 instruction_label.pack(pady=10)
 
 character_entry = tk.Entry(app, font=('Arial', 14))
